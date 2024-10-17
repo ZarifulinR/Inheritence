3e7381fd162ae3af48a7be2dd5d1182d64e6a39f
ASUTP <ASUTP@DESKTOP-SSAKK7M>
2024-10-17 22:01:37 +0300

    RectangleDone

---
+++ AbstruractGeometry/Rectangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace AbstractGeometry
+{
+
+	class Rectangle:Shape
+	{
+		double width;
+		double heigth;
+		public double Width
+		{
+			get => width;
+			set => width = SizeFilter(value);
+		}
+		public double Heigth
+		{
+			get => heigth;
+			set => heigth = SizeFilter(value);		
+		}
+		public Rectangle
+			(
+			double width, double heigth,int startX, 
+			int startY, int lineWidth, Color color
+			):base(startX,startY,lineWidth,color)
+		{
+			Width = width;
+			Heigth = heigth;
+		}
+		public override double GetArea() => Width * Heigth;
+
+		public override double GetPerimeter() => 2 * (Width + Heigth);
+
+		public override void Draw(PaintEventArgs e)
+		{
+			Pen pen = new Pen(Color, LineWidth);
+			e.Graphics.DrawRectangle(pen, StartX, StartY, (int)Width, (int)Heigth);
+		}
+
+	}
+}
+
