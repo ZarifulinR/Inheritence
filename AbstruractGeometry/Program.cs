4a6c4770abde78a19f98bc993a861374b36b8112
ASUTP <ASUTP@DESKTOP-SSAKK7M>
2024-10-22 21:27:13 +0300

    TrianglesINprogrress

--- AbstruractGeometry/Program.cs
+++ AbstruractGeometry/Program.cs
@@ -24,11 +24,14 @@ namespace AbstractGeometry
 			
 			Rectangle rectangle = new Rectangle(100, 50, 450, 100, 3, System.Drawing.Color.Red);
 			rectangle.Info(e);
-			Circle circle = new Circle(70, 570, 95, 3, System.Drawing.Color.Gold);
-			Square square = new Square(75, 800, 100, 1, System.Drawing.Color.Aqua);
+			Circle circle = new Circle(30, 570, 95, 3, System.Drawing.Color.Gold);
+			Square square = new Square(75, 750, 95, 1, System.Drawing.Color.Aqua);
 			square.Info(e);
 			circle.Info(e);
-			
+			IsoscalesTriangle i_triangle = new IsoscalesTriangle(80, 80, 650, 85, 3, Color.Green);
+			i_triangle.Info(e);
+			EquilLateralTriangle e_triangle = new EquilLateralTriangle(100, 450, 160, 3, Color.GreenYellow);
+			e_triangle.Info(e);
 		}
 		[DllImport("kernel32.dll")]
 		public static extern IntPtr GetConsoleWindow();
--- AbstruractGeometry/Triangle.cs
+++ AbstruractGeometry/Triangle.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace AbstractGeometry
 {
-	class Triangle
+	abstract class Triangle: Shape
 	{
+		public abstract double GetHwight();
+		public Triangle(int startX, int startY, int lineWidth, Color color) : base(startX, startY, lineWidth, color) { }
+		public override void Info(PaintEventArgs e)
+		{
+			Console.WriteLine($"Высота треугольника:{GetHwight()} ");
+			base.Info(e);
+		}
 	}
 }
